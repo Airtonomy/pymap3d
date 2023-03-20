6da21328138d6a77c5ca4807b5b4d6f310a8c652
Aaron Roller <aaron.roller@airtonomy.ai>
2023-03-20 07:55:30 -0700

    generated debian package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+	version="3.0.1",
+    name = "pymap3d",
+    description = "pure Python (no prereqs) coordinate conversions, following convention of several popular Matlab routines.",
+	package_dir={"": "src"},
+    packages=["pymap3d"]
+
+)
